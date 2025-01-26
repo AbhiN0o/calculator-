4829abff6411b316de3254b646f53e8c787ec29a
Abhinav <dhruvsahu550@gmail.com>
2025-01-26 22:20:27 +0530

    added state to add all the digits

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,37 @@
 import { useState } from 'react'
 import "./index.css"
 function App() {
-  const [count, setCount] = useState(0)
+  const [operands,setOperands]=useState({previousOperand:``,currentOperand:``})
+  function addDigits(digit){
+    setOperands(prevOperand=>({...prevOperand,currentOperand:`${prevOperand.currentOperand}${digit}`}))
+  }
+  
 
+
+  console.log(operands)
   return (
     <div className='calculator-grid'>
       <div className='output'>
           <div className='previous-operand'></div>
-          <div className='current-operand'></div>
+          <div className='current-operand'>{operands.currentOperand}</div>
       </div> 
       <button className='span-two'>AC</button>
-      <button>DEL</button>
+      <button >DEL</button>
       <button>/</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
+      <button onClick={()=>{addDigits("1")}}>1</button>
+      <button onClick={()=>{addDigits("2")}}>2</button>
+      <button onClick={()=>{addDigits("3")}}>3</button>
       <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
+      <button onClick={()=>{addDigits("4")}}>4</button>
+      <button onClick={()=>{addDigits("5")}}>5</button>
+      <button onClick={()=>{addDigits("6")}}>6</button>
       <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
+      <button onClick={()=>{addDigits("7")}}>7</button>
+      <button onClick={()=>{addDigits("8")}}>8</button>
+      <button onClick={()=>{addDigits("9")}}>9</button>
       <button>-</button>
       <button>.</button>
-      <button>0</button>
+      <button onClick={()=>{addDigits("0")}}>0</button>
       <button className='span-two'>=</button>
     </div>
   )
